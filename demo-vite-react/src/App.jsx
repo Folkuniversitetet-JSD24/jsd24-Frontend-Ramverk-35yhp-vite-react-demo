b9c8065946491109a4d7634b7ed9f08597f3fc93
Mandus Lindström <mandus85@hotmail.com>
2025-01-27 15:51:31 +0100

    Done with code along, time for exercises from another repo.

---
+++ demo-vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello JSD24</h1>
+      <p>Hej igen</p>
+    </main>
+  );
+}
+
+export default App;
---
+++ demo-vite-react/src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>Här är vår footer</footer>;
+}
+
+export default Footer;
